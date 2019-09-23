bdcd22f5e56ab4d568a033f8bfa8ad19cbf2f321
鼎岳 <dingyue@taobao.com>
2019-09-23 19:12:52 +0800

    src init

---
+++ src/main/java/com/alibaba/dingyue/k8sweb/K8sWebApplication.java
@@ -0,0 +1,13 @@
+package com.alibaba.dingyue.k8sweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alibaba/dingyue/k8sweb/K8sWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba.dingyue.k8sweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
