daa9f3e7ea04e4d41e05afc202e23662ad69242a
lilongthinker <lilongthinker@163.com>
2019-10-31 21:54:16 +0800

    打印header信息

--- src/main/java/com/alibaba/dingyue/k8sweb/hello/HelloController.java
+++ src/main/java/com/alibaba/dingyue/k8sweb/hello/HelloController.java
@@ -2,14 +2,19 @@ package com.alibaba.dingyue.k8sweb.hello;
 
 import com.alibaba.dingyue.k8sweb.data.Student;
 import com.alibaba.dingyue.k8sweb.repo.StudentRepo;
+import com.google.common.base.Joiner;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
 
 @RestController
 @Slf4j
@@ -35,7 +40,6 @@ public class HelloController {
         if(now%10 == 0){
             throw new RuntimeException("10 nanos exception. this is by designed");
         }
-
         return "greeting from spring cloud. message in env:"+env+"\n";
     }
 
@@ -48,6 +52,16 @@ public class HelloController {
     public List<Student> list() {
         return studentRepo.findAll();
     }
+
+    @RequestMapping("/headers")
+    public String headers(@RequestHeader HttpHeaders headers){
+        StringBuilder sbuilder = new StringBuilder();
+        for(Map.Entry<String,List<String>> entry : headers.entrySet()){
+            sbuilder.append(entry.getKey()).append(":").append( Joiner.on(",").join(entry.getValue())).append("---------------\n");
+        }
+        return sbuilder.toString();
+    }
+
 
     @RequestMapping("/readiness/check")
     public String readinessCheck(){
