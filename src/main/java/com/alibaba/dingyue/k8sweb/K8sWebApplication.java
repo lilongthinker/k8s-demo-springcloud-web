c1d163cb4805e60da21cc1041b473091ee3c2c08
lilongthinker <lilongthinker@163.com>
2020-09-01 10:23:42 +0800

    添加信号感受程序

--- src/main/java/com/alibaba/dingyue/k8sweb/K8sWebApplication.java
+++ src/main/java/com/alibaba/dingyue/k8sweb/K8sWebApplication.java
@@ -1,13 +1,39 @@
 package com.alibaba.dingyue.k8sweb;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
 
 @SpringBootApplication
-public class K8sWebApplication {
+@Slf4j
+public class K8sWebApplication  {
 
 	public static void main(String[] args) {
+		DebugSignalHandler.listenTo("HUP");
+		DebugSignalHandler.listenTo("INT");
+//        DebugSignalHandler.listenTo("KILL");
+		DebugSignalHandler.listenTo("TERM");
+
 		SpringApplication.run(K8sWebApplication.class, args);
 	}
 
+
+}
+
+
+class DebugSignalHandler implements SignalHandler {
+	public static void listenTo(String name) {
+		Signal signal = new Signal(name);
+		Signal.handle(signal, new DebugSignalHandler());
+	}
+
+	public void handle(Signal signal) {
+		System.out.println("Signal: " + signal);
+		if (signal.toString().trim().equals("SIGTERM")) {
+			System.out.println("SIGTERM raised, terminating...");
+			System.exit(1);
+		}
+	}
 }
