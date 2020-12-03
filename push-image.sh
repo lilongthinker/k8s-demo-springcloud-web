ed97e770ccdafe8be21df3df9d1f929916637cb6
lilongthinker <lilongthinker@163.com>
2020-12-03 14:15:38 +0800

    image build

--- push-image.sh
+++ push-image.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh
 #docker login --username=dingyue_cloudnative registry.cn-beijing.aliyuncs.com
 version=$1
+repo_domain=second-instance-registry-vpc.cn-beijing.cr.aliyuncs.com
+repo_ns=first_ns
 imageName=k8s-web
-mcp
+mvn clean package -Dmaven.test.skip=true
 docker build -t $imageName:$version ./
 ImageId=`docker build -t $imageName:v1 ./ |grep "Successfully built"|awk -F" " '{print $3}'`
-docker tag $ImageId registry.cn-beijing.aliyuncs.com/k8s-demo-dingyue/$imageName:$version
-docker push registry.cn-beijing.aliyuncs.com/k8s-demo-dingyue/$imageName:$version
+docker tag $ImageId $repo_domain/$repo_ns/$imageName:$version
+docker push $repo_domain/$repo_ns/$imageName:$version
 
 date
