35f155a3067c437dd54dc2702129cf8c5fc27a5a
lilongthinker <lilongthinker@163.com>
2022-08-05 17:40:19 +0800

    make the test pass

--- src/test/java/com/alibaba/dingyue/k8sweb/K8sWebApplicationTests.java
+++ src/test/java/com/alibaba/dingyue/k8sweb/K8sWebApplicationTests.java
@@ -1,9 +1,7 @@
 package com.alibaba.dingyue.k8sweb;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class K8sWebApplicationTests {
 
 	@Test
